02284c6d0d52a2d2c33df8400c4ccee09c6ac73a
Preranabrainvire <preranababar9403@gmail.com>
2024-09-20 18:19:22 +0530

    responsive fully and opacity set

--- src/components/Host.jsx
+++ src/components/Host.jsx
@@ -14,7 +14,7 @@ const Host = () => {
                    height={1}
       />
 
-<div className="absolute font-rufina max-xl:w-1/2 max-2xl:w-1/2 w-1/3 max-lg:w-1/2  max-md:w-2/3 max-sm:w-4/5 bg-white flex flex-col items-center py-10 rounded-lg px-3 top-[30%] max-xl:left-[25%] max-lg:left-[25%] max-md:left-[18%] max-sm:left-[10%] max-2xl:left-[23%]   left-[32%]   opacity-75 ">
+<div className="absolute font-rufina max-xl:w-1/2 max-2xl:w-1/2 w-1/3 max-lg:w-1/2  max-md:w-2/3 max-sm:w-4/5 bg-white flex flex-col items-center py-10 rounded-lg px-3 top-[30%] max-xl:left-[25%] max-lg:left-[25%] max-md:left-[18%] max-sm:left-[10%] max-2xl:left-[23%]   left-[32%]   opacity-85 ">
         <p className="text-lg text-primary1">Join Us</p>
         <h3 className="lg:text-4xl text-3xl font-bold  pb-10">Become a host</h3>
 
