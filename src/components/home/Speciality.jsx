06f1d88626dbe2f732e9c22a168849352b9e2c33
Preranabrainvire <preranababar9403@gmail.com>
2024-09-22 11:06:00 +0530

    speciality section responsive

--- src/components/home/Speciality.jsx
+++ src/components/home/Speciality.jsx
@@ -1,100 +1,64 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
+
 
 const Speciality = () => {
   return (
-        <section className="py-20  bg-gray-200">
-    <div className="max-width my-20">
-    <div className="font-rufina text-center mb-14 ">
+    <section className="py-20  bg-gray-200">
+      <div className="max-width my-20">
+        <div className="font-rufina text-center mb-14 ">
           <h2 className="text-center lg:text-5xl text-3xl  pb-5">
             Our Properties Specialities
           </h2>
-          </div>
-
-
-          <div className="flex justify-evenly gap-10 max-lg:flex-col items-center ">
-                <div className="flex flex-col items-start  font-rufina">
-                <Image
-                src="/icons/s1.svg"
-                alt="Hero image"
-                className="w-10 h-10 "
-                width={10}
-                height={10}
-              />
-
-       
-              <h4 className="text-3xl pt-8">Seafront</h4>
-              <p className="text-xl pt-3 ">have a look</p>
-              
-
-                </div>
-            
-
-
-
-                <div className="flex flex-col items-start font-rufina">
-                <Image
-                src="/icons/s2.svg"
-                alt="Hero image"
-                className="w-10 h-10 "
-                width={10}
-                height={10}
-              />
-
-              <h4 className="text-3xl pt-8">Pet friendly</h4>
-              <p className="text-xl pt-3">pets allowed</p>
-                </div>
-
-
-
-                <div className="flex flex-col items-start font-rufina">
-                <Image
-                src="/icons/s3.svg"
-                alt="Hero image"
-                className="w-10 h-10 "
-                width={10}
-                height={10}
-              />
-
-              <h4 className="text-3xl pt-8">Electric car</h4>
-              <p className="text-xl pt-3">charge your car</p>
-                </div>
-
-
-                
-
-                <div className="flex flex-col items-start font-rufina">
-                <Image
-                src="/icons/s4.svg"
-                alt="Hero image"
-                className="w-10 h-10 "
-                width={10}
-                height={10}
-              />
-
-              <h4 className="text-3xl pt-8">Fitness/Gym</h4>
-              <p className="text-xl pt-3">workout</p>
-                </div>
-
-
-                
-
-                <div className="flex flex-col items-start font-rufina">
-                <Image
-                src="/icons/s5.svg"
-                alt="Hero image"
-                className="w-10 h-10 "
-                width={10}
-                height={10}
-              />
-
-              <h4 className="text-3xl pt-8">Boot moorning</h4>
-              <p className="text-xl pt-3">take a strip</p>
-                </div>
-          </div>
         </div>
-    </section>
-  )
-}
 
-export default Speciality
+        <div className="flex justify-evenly max-md:w-full gap-10 max-lg:flex-col items-center ">
+          {special.map((item, index) => (
+            <div key={index} className="flex max-md:w-1/2 flex-col items-center justify-center  font-rufina">
+              <Image
+                src={item.icon}
+                alt="Hero image"
+                className="w-10 h-10 "
+                width={10}
+                height={10}
+              />
+
+              <h4 className="lg:text-3xl text-center text-2xl pt-8">{item.title}</h4>
+              <p className="text-xl pt-3 ">{item.msg}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Speciality;
+
+const special = [
+  {
+    icon: "/icons/s1.svg",
+    title: "Seafront",
+    msg: "have a look",
+  },
+  {
+    icon :  "/icons/s2.svg",
+    title : "Pet friendly",
+    msg : "pets allowed"
+  },
+  {
+    icon :  "/icons/s3.svg",
+    title : "Electric car",
+    msg : "charge your car"
+  },
+  {
+    icon :  "/icons/s4.svg",
+    title : "Fitness/Gym",
+    msg : "workout"
+  },
+  {
+    icon :  "/icons/s5.svg",
+    title : "Boot moorning",
+    msg : "take a strip"
+  },
+];
