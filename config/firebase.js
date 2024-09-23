e94e379c391bd9c86d638ed575469b77a35528e4
Preranabrainvire <preranababar9403@gmail.com>
2024-09-23 20:29:41 +0530

    firebase configurated in config folder

---
+++ config/firebase.js
@@ -0,0 +1,33 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import "firebase/compat/firestore";   //  the Firestore database service
+import "firebase/compat/storage";     //  Firebase's storage service for file uploads
+import { getAuth } from "firebase/auth";     //  Firebase's authentication service 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdf7l5oorevTHgCKg7vSOZrGBTl9h5vCc",
+  authDomain: "rental-villas.firebaseapp.com",
+  projectId: "rental-villas",
+  storageBucket: "rental-villas.appspot.com",
+  messagingSenderId: "720225717298",
+  appId: "1:720225717298:web:52949ce3e55547575c002b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const storage = firebase.storage();
+const auth = firebase.auth();
+
+
+
+
+
+
+
+
