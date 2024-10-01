a4310fada5811ea1ccbfa92909d94f271d5a96f4
Preranabrainvire <preranababar9403@gmail.com>
2024-10-01 20:28:28 +0530

    created date component for pdp

--- src/app/villadetails/[id]/page.jsx
+++ src/app/villadetails/[id]/page.jsx
@@ -2,11 +2,14 @@ import Villapdp from '@/components/product/Villapdp'
 import Scene from '@/components/home/Scene'
 import React from 'react'
 
+import Date from '@/components/product/Date'
+
 const page = () => {
   return (
 
     <div>
       <Villapdp/>
+      <Date/>
       <Scene/>
     </div>
   )
---
+++ src/components/product/Date.jsx
@@ -0,0 +1,18 @@
+
+"use client";
+import React from 'react'
+
+
+const Date = () => {
+      
+  return (
+        <section className="py-20 bg-[#f4f4f1]">
+                 <div className="max-width">
+            
+                        </div>
+        </section>
+   
+  )
+};
+
+export default Date
